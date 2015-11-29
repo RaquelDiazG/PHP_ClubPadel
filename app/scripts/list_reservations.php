acf592441bc9bfd45e1049da9433449d0b8cd416
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-11-29 13:45:58 +0100

    Creado script para listar reservas

---
+++ app/scripts/list_reservations.php
@@ -0,0 +1,17 @@
+<?php
+
+// app/scripts/list_groups.php
+
+require_once __DIR__ . '/../../config/bootstrap.php';
+
+$entityManager = GetEntityManager();
+
+$reservationRepository = $entityManager->getRepository('AppBundle\Entity\Reservation');
+$reservations = $reservationRepository->findAll();
+
+echo sprintf("  %2s %20s %10s %10s\n", 'Id', 'Datetime', "Court", 'User');
+foreach ($reservations as $reservation) {
+    echo sprintf("- %2d %20s %10s %10s\n", $reservation->getId(), $reservation->getDatetime()->format('d-m-Y H:i:s'), $reservation->getCourt(), $reservation->getUser());
+}
+
+echo "\nTotal: " . count($reservations) . " courts.\n\n";
