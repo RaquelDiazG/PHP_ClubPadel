f04afd4f283c4dc78778bc3ba0ea203664d1aeb2
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-11-29 14:56:17 +0100

    Creado script para modificar pistas

---
+++ app/scripts/update_court.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../../config/bootstrap.php';
+
+//Check usage
+if ($argc !== 3) {
+    echo "Usage:\n   php ", basename($argv[0]), ' id active' . PHP_EOL;
+    exit();
+}
+
+//Get court (object)
+$entityManager = GetEntityManager();
+$courtRepository = $entityManager->getRepository('AppBundle\Entity\Court');
+$court = $courtRepository->find(intval($argv[1]));
+
+//Update court (object)
+$court->setActive($argv[2]);
+
+//Update from BBDD
+$entityManager->flush();
