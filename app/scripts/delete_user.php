84a6decf9db94d93504e8a432f8c165d133bff57
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-11-29 14:02:41 +0100

    Creado script para eliminar usuarios

---
+++ app/scripts/delete_user.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../../config/bootstrap.php';
+
+//Check usage
+if ($argc !== 2) {
+    echo "Usage:\n   php ", basename($argv[0]), ' id' . PHP_EOL;
+    exit();
+}
+
+//Get user (object)
+$entityManager = GetEntityManager();
+$userRepository = $entityManager->getRepository('AppBundle\Entity\User');
+$user = $userRepository->find(intval($argv[1]));
+
+//Remove from BBDD
+$entityManager->remove($user);
+$entityManager->flush();
