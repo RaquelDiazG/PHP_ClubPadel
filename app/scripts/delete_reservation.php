810fa9cf76b86f4530c4ee4ff9af006bea694540
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-11-29 14:02:31 +0100

    Creado script para eliminar reservas

---
+++ app/scripts/delete_reservation.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../../config/bootstrap.php';
+
+//Check usage
+if ($argc !== 2) {
+    echo "Usage:\n   php ", basename($argv[0]), ' id' . PHP_EOL;
+    exit();
+}
+
+//Get reservation (object)
+$entityManager = GetEntityManager();
+$reservationRepository = $entityManager->getRepository('AppBundle\Entity\Reservation');
+$reservation = $reservationRepository->find(intval($argv[1]));
+
+//Remove from BBDD
+$entityManager->remove($reservation);
+$entityManager->flush();
